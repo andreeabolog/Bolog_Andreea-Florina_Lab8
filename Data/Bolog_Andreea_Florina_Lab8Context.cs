f29ab7dca0037dec0f7d2461b0aeca1615e66daf
Andreea Bolog <andreeabolog84@yahoo.com>
2020-11-26 13:43:48 +0200

    Add project files.

---
+++ Data/Bolog_Andreea_Florina_Lab8Context.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Bolog_Andreea_Florina_Lab8.Models;
+
+namespace Bolog_Andreea_Florina_Lab8.Data
+{
+    public class Bolog_Andreea_Florina_Lab8Context : DbContext
+    {
+        public Bolog_Andreea_Florina_Lab8Context (DbContextOptions<Bolog_Andreea_Florina_Lab8Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Bolog_Andreea_Florina_Lab8.Models.Book> Book { get; set; }
+
+        public DbSet<Bolog_Andreea_Florina_Lab8.Models.Publisher> Publisher { get; set; }
+        public DbSet<Bolog_Andreea_Florina_Lab8.Models.Category> Category { get; set; }
+    }
+}
